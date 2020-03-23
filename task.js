68252e8c3dc99a3bbf5add26ff18a34d10109e02
Arpine Ktoyan <arpine.ktoyan@gmail.com>
2020-03-23 23:07:37 +0400

    Task 5

--- task.js
+++ task.js
@@ -17,71 +17,73 @@ let appDate ={
     expenses:{ },
     optionalExpenses:{},
     income:[],
-    savings: true
+    savings: true,
+    chooseExpenses: function(){
+        for(let i = 0; i < 2; i++){
+            let answer1 = prompt("Введите обязательную статью расходов в этом месяце","");
+            let answer2 = prompt("Во сколько обойдется?","");
+            
+            if((typeof(answer1)) === 'string' && (typeof(answer1)) !== null &&
+            (typeof(answer2)) === 'string' && (typeof(answer2)) !== null &&
+            answer1 != '' && answer2 != '' && answer1.length <50){
+                console.log("done");
+                appDate.expenses[answer1] = answer2;
+            }else{
+                i = i -1;
+            }  
+        }
+
+    },
+    detectDayBudget: function(){
+        appDate.moneyPerDay = appDate.bjudjet/30
+        alert("бюджет на 1 день: " + appDate.moneyPerDay);
+    },
+    detectLevel: function(){
+        if(appDate.moneyPerDay < 100){
+            console.log("Minimum");
+        }else if(appDate.moneyPerDay > 100 && appDate.moneyPerDay < 2000){
+            console.log("Medimum");
+        }else if(appDate.moneyPerDay > 2000){
+            console.log("Maximum");
+        }else{
+            console.log("Mistake");
+        }
+    },
+    checkSavings: function(){
+        if(appDate.savings == true){
+            let save = +prompt('Kakova summa nakopleni?','');
+            let percent = +prompt('Pod kakoy procent','');
+            appDate.monthIncome = save/100/12*percent;
+            alert('Doxod v mesyac s vawego depozita: ' + appDate.monthIncome);  
+        }
+    },
+    chooseOptExpenses: function(){
+        for(let i = 1; i < 3; i++){
+            let opt = +prompt('Neobjazatelnie rasxodi','');
+            appDate.optionalExpenses[i] = opt;
+        }
+    },
+    chooseIncome: function(){
+        let items = prompt("Dopolnitelnie doxodi(Perechislit cherez zapyatuyu","");
+        appDate.income = items.split(', ');
+        appDate.income.push(prompt("Mojet cto-to ewe?"));
+        appDate.income.sort();
+    }
 };
 
-function chooseExpenses(){
-    for(let i = 0; i < 2; i++){
-        let answer1 = prompt("Введите обязательную статью расходов в этом месяце","");
-        let answer2 = prompt("Во сколько обойдется?","");
-        
-        if((typeof(answer1)) === 'string' && (typeof(answer1)) !== null &&
-        (typeof(answer2)) === 'string' && (typeof(answer2)) !== null &&
-        answer1 != '' && answer2 != '' && answer1.length <50){
-            console.log("done");
-            appDate.expenses[answer1] = answer2;
-        }else{
-            i = i -1;
-        }  
-    }
-}
-
-chooseExpenses();
 
 
-appDate.moneyPerDay = (appDate.bjudjet/30).toFixed();
-alert("бюджет на 1 день: " + appDate.moneyPerDay);
-
-if(appDate.moneyPerDay < 100){
-    console.log("Minimum");
-}else if(appDate.moneyPerDay > 100 && appDate.moneyPerDay < 2000){
-    console.log("Medimum");
-}else if(appDate.moneyPerDay > 2000){
-    console.log("Maximum");
-}else{
-    console.log("Mistake");
-}
-
-//zadanie 4
-
-function detectDayBudget(){
-    appDate.moneyPerDay = appDate.bjudjet/30
-alert("бюджет на 1 день: " + appDate.moneyPerDay);
-}
-
-function detectLevel(){
-    if(appDate.moneyPerDay < 100){
-        console.log("Minimum");
-    }else if(appDate.moneyPerDay > 100 && appDate.moneyPerDay < 2000){
-        console.log("Medimum");
-    }else if(appDate.moneyPerDay > 2000){
-        console.log("Maximum");
-    }else{
-        console.log("Mistake");
-    }
-}
-
-function chooseOptExpenses(){
-    return appDate.optionalExpenses;
-}
+// if(appDate.moneyPerDay < 100){
+//     console.log("Minimum");
+// }else if(appDate.moneyPerDay > 100 && appDate.moneyPerDay < 2000){
+//     console.log("Medimum");
+// }else if(appDate.moneyPerDay > 2000){
+//     console.log("Maximum");
+// }else{
+//     console.log("Mistake");
+// }
 
 
-function checkSavings(){
-    if(appDate.savings == true){
-        let save = +prompt('Kakova summa nakopleni?','');
-        let percent = +prompt('Pod kakoy procent','');
-        appDate.monthIncome = save/100/12*percent;
-        alert('Doxod v mesyac s vawego depozita: ' + appDate.monthIncome);  
-    }
-}
-checkSavings();
+    // while(chooseIncome == '' || typeof chooseIncome !== 'string' || chooseIncome == null){
+    //     chooseIncome = prompt("Dopolnitelnie doxodi","")
+    // }
